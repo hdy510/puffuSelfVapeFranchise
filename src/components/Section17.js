be7b8064ebeec606f5ae344cc018154ee1c7fe72
hdy510 <157979173+hdy510@users.noreply.github.com>
2025-08-27 13:31:01 +0900

    sns 링크연결

--- src/components/Section17.js
+++ src/components/Section17.js
@@ -3,6 +3,7 @@ import puffu from "../resources/puffu.png";
 import kakaotalk from "../resources/kakaotalk.png";
 import youtube from "../resources/youtube.png";
 import instagram from "../resources/instagram.png";
+import blog from "../resources/blog.png";
 
 
 function Section17 () {
@@ -17,17 +18,29 @@ function Section17 () {
           </div>
         </div>
         <ul className={styles.snsBox}>
-          <li className={styles.snsCard}>
+          <li className={styles.snsCard}
+            onClick={() => window.open("https://pf.kakao.com/_LUaGn", "_blank")}
+          >
             <img src={kakaotalk} alt="카카오톡 문의" className={styles.icon} />
             <p className={styles.snsName}>카카오톡 문의</p>
           </li>
-          <li className={styles.snsCard}>
+          <li className={styles.snsCard}
+            onClick={() => window.open("https://www.instagram.com/puffu_official/?hl=ko", "_blank")}
+          >
+            <img src={instagram} alt="푸푸 인스타그램" className={styles.icon} />
+            <p className={styles.snsName}>푸푸 인스타그램</p>
+          </li>
+          <li className={styles.snsCard}
+            onClick={() => window.open("https://www.youtube.com/@PuffuOfficial", "_blank")}
+          >
             <img src={youtube} alt="푸푸 유튜브" className={styles.icon} />
             <p className={styles.snsName}>푸푸 유튜브</p>
           </li>
-          <li className={styles.snsCard}>
-            <img src={instagram} alt="푸푸 인스타그램" className={styles.icon} />
-            <p className={styles.snsName}>푸푸 인스타그램</p>
+          <li className={styles.snsCard}
+            onClick={() => window.open("https://blog.naver.com/puffuofficial", "_blank")}
+          >
+            <img src={blog} alt="푸푸 블로그" className={styles.icon} />
+            <p className={styles.snsName}>푸푸 블로그</p>
           </li>
         </ul>
       </div>
